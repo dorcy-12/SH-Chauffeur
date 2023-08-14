ebc446df69a8e6682d7776280f188dc24e004be2
dorcy-12 <agapedorcy@gmail.com>
2023-08-14 12:58:45 +0200

    first actual commmit

---
+++ screens/login.js
@@ -0,0 +1,53 @@
+// screens/LoginScreen.js
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+
+function LoginScreen({ navigation }) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Replace with your authentication logic
+    if (username === 'user' && password === 'password') {
+      navigation.navigate('Home');
+    } else {
+      alert('Invalid credentials');
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text>Login</Text>
+      <TextInput
+        placeholder="Username"
+        value={username}
+        onChangeText={setUsername}
+        style={styles.input}
+      />
+      <TextInput
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        
+        style={styles.input}
+      />
+      <Button title="Login" onPress={handleLogin} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  input: {
+    width: '80%',
+    padding: 10,
+    borderWidth: 1,
+    marginBottom: 10,
+  },
+});
+
+export default LoginScreen;
