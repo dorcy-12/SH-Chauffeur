83a0808a324a385aef719b76ee19b3d47caed77d
dhakizimana <agapedorcy@gmail.com>
2023-08-18 13:03:28 +0200

    stack created and login improved

--- Navigator/StackNavigator.js
+++ Navigator/StackNavigator.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
-import LoginScreen from '../screens/login';
-import HomeScreen from '../screens/home';
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import LoginScreen from "../screens/login";
+import HomeScreen from "../screens/home";
 
 const Stack = createStackNavigator();
 
 function StackNavigator() {
   return (
     <Stack.Navigator>
+      <Stack.Screen name="login" component={LoginScreen} options={{headerShown: false}}/>
 
-      <Stack.Screen name="login" component={LoginScreen} />
       <Stack.Screen name="Home" component={HomeScreen} />
     </Stack.Navigator>
   );
 }
-
 
 export default StackNavigator;
--- screens/login.js
+++ screens/login.js
@@ -1,40 +1,51 @@
-// screens/LoginScreen.js
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, Image, StyleSheet } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, Image, StyleSheet, SafeAreaView } from 'react-native';
+import { Feather } from "@expo/vector-icons"
 
 function LoginScreen({ navigation }) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [isPinVisible, setIsPinVisible] = useState(false);
+
 
   const handleLogin = () => {
     // Replace with your authentication logic
-    if (username === 'user' && password === 'password') {
       navigation.navigate('Home');
-    } else {
-      alert('Invalid credentials');
-    }
   };
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Image source={require('../assets/logo-no.png')} style={styles.logo} />
       <View style={styles.form}>
-        <TextInput
-          placeholder="Username"
+        <View style = {styles.inputContainer}>
+          <TextInput
+          placeholder="Fahrer-Nr"
           value={username}
           onChangeText={setUsername}
           style={styles.input}
         />
-        <TextInput
-          placeholder="Password"
-          value={password}
-          onChangeText={setPassword}
-          secureTextEntry
-          style={styles.input}
-        />
-        <Button title="Login" onPress={handleLogin} />
+        </View>
+        
+        <View style={styles.inputContainer}>
+            <TextInput
+              placeholder="PIN"
+              value={password}
+              onChangeText={setPassword}
+              secureTextEntry={!isPinVisible}
+              style={styles.input}
+            />
+            <TouchableOpacity style={styles.pinToggle} onPress={() => setIsPinVisible(!isPinVisible)}>
+              <Feather
+                name={isPinVisible ? "eye-off" :"eye"}
+                size = {24}
+              />
+            </TouchableOpacity>
+          </View>
+        <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
       </View>
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -43,22 +54,50 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#F7F7F7',
   },
   logo: {
-    width: 150,
-    height: 150,
+    width: 200,
+    height: 200,
     resizeMode: 'contain',
-    marginBottom: 20,
+    marginBottom: 40,
   },
   form: {
-    width: '80%',
+    width: '85%',
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'stretch',
+    width: '100%',
+    borderWidth: 1,
+    borderRadius: 8,
+    borderColor: '#E0E0E0', 
+    marginBottom: 15,
+    backgroundColor: 'white',
+  },
+  pinToggle: {
+    position: 'absolute',
+    right: 10,
+    top: '50%',
+    transform: [{ translateY: -12 }],  // assuming half the size of the icon
   },
   input: {
+    flex: 1,
+    padding: 12,
+    paddingRight: 40,  // to ensure text doesn't overlap with the icon
+  },
+  loginButton: {
     width: '100%',
-    padding: 10,
-    borderWidth: 1,
-    marginBottom: 10,
-    borderRadius: 5,
+    padding: 12,
+    backgroundColor: '#3498db',
+    borderRadius: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
   },
 });
+
 export default LoginScreen;
