af1332442f2769e4f1ee23d84057533615689565
dhakizimana <agapedorcy@gmail.com>
2023-10-11 09:20:15 +0200

    home screen updated mock trips

--- screens/home.js
+++ screens/home.js
@@ -14,18 +14,35 @@ import {
 import Card from "../Components/Card";
 import { useTheme } from "../context/ThemeContext";
 import { fetchTrips } from "../service/authservice";
+import * as SecureStore from 'expo-secure-store';
+
+
 const mockTrips = [
+
   {
     id: 1,
     vehicle: { vehicle_number: "1234" },
+    start_location: "",
     end_location: "Davenportplatz",
     start_time: new Date().toISOString(),
+    driven_time: 0,
+    distance: 0,
+    stopped:"",
+    userPath: null,
+    is_completed: false
   },
   {
+
     id: 2,
     vehicle: { vehicle_number: "5678" },
-    end_location: "Frankfurt",
+    start_location: "",
+    end_location: "Opelstrase, 12",
     start_time: new Date().toISOString(),
+    driven_time: 0,
+    distance: 0,
+    stopped:"",
+    userPath: null,
+    is_completed:false
   },
   // ... Add more mock trips as needed
 ];
@@ -36,15 +53,18 @@ function HomeScreen({ navigation }) {
 
   const [refreshing, setRefreshing] = useState(false); // Add this line
 
-
   useEffect(() => {
     loadTrips();
   }, []);
+  
 
   const loadTrips = async () => {
     setRefreshing(true);
     try {
-      const fetchedTrips = mockTrips
+      await SecureStore.setItemAsync('trips', JSON.stringify(mockTrips));
+      console.log("updated the trips successfully");
+      const fetchedTripsData = await SecureStore.getItemAsync("trips");
+      const fetchedTrips = fetchedTripsData ? JSON.parse(fetchedTripsData) : [];
       setTrips(fetchedTrips);
     } catch (error) {
       console.error("Error loading trips:", error);
@@ -61,18 +81,18 @@ function HomeScreen({ navigation }) {
       {/* Content */}
 
       <FlatList
-        data={trips}
+        data={trips.filter(trip => !trip.is_completed)}
         keyExtractor={(item) => item.id.toString()}
         renderItem={({ item }) => (
           <TouchableOpacity
             onPress={() =>
-              navigation.navigate("Map", { location: item.end_location })
+              navigation.navigate("Map", {tripId:item.id})
             }
           >
             <Card
-              number={item.vehicle.vehicle_number}
+              number={item.is_completed ? "true" : "false"}
               location={item.end_location}
-              time={new Date(item.start_time).toLocaleTimeString().slice(0, 4)}
+              time={new Date(item.start_time).toLocaleTimeString().slice(0,5)}
               date={new Date(item.start_time).toLocaleDateString()}
             />
           </TouchableOpacity>
