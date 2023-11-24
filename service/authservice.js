fa5f18028a10f2c13656dcce15c72d52cfd4e20c
dhakizimana <agapedorcy@gmail.com>
2023-11-24 12:39:20 +0100

    auth service updated

--- service/authservice.js
+++ service/authservice.js
@@ -34,10 +34,9 @@ export async function loginUser(username, password) {
     throw error;
   }
 }
-export async function fetchEmployeeProfile(userId) {
+export async function fetchEmployeeProfile(userId, password) {
   const url = `${BASE_URL}/jsonrpc`;
-  const password = await SecureStore.getItemAsync("password");
-  console.log("employee profile " + userId + " " + password) ;
+  const pin = await SecureStore.getItemAsync("password");
   const payload = {
     jsonrpc: "2.0",
     method: "call",
@@ -47,11 +46,11 @@ export async function fetchEmployeeProfile(userId) {
       args: [
         DB_NAME,
         userId,
-        await SecureStore.getItemAsync("password"),
+        pin,
         "hr.employee",
         "search_read",
         [[["user_id", "=", parseInt(userId, 10)]]],
-        { fields: ["name", "image_1920", "work_email"] },  // Adjust field names if necessary
+        { fields: ["id", "name", "image_1920", "work_email"] }, // Adjust field names if necessary
       ],
     },
     id: Math.floor(Math.random() * 100) + 1,
@@ -63,19 +62,104 @@ export async function fetchEmployeeProfile(userId) {
     console.log(employeeData);
 
     if (employeeData && employeeData.length > 0) {
-      const { name, image_1920, work_email } = employeeData[0];
-      return { name, profilePicture: image_1920, work_email };
+      const { id, name, image_1920, work_email } = employeeData[0];
+      await SecureStore.setItemAsync("employeeId", id.toString());
+      return { id, name, profilePicture: image_1920, work_email };
     }
     return null;
   } catch (error) {
     console.error("Error in fetchEmployeeProfile", error);
-    throw error; 
+    throw error;
   }
-}  
- 
-export async function fetchVehicleServices(userId) {
+}
+
+export async function fetchVehicleServices(userId, serviceState, password) {
   const url = `${BASE_URL}/jsonrpc`;
-  const password = await SecureStore.getItemAsync("password");
+  console.log(userId);
+  const pin = await SecureStore.getItemAsync("password");
+ 
+  const payload = {
+    jsonrpc: "2.0",
+    method: "call",    
+    params: {
+      service: "object",
+      method: "execute_kw",
+      args: [
+        DB_NAME,
+        userId,
+        pin,
+        "fleet.vehicle.log.services",
+        "search_read",
+        [[["state", "=", serviceState]]],
+        {
+          fields: [
+            "service_type_id",
+            "vehicle_id",
+            "date",
+            "purchaser_id",
+            "state",
+            "description",
+            "notes",
+          ],
+        },
+      ],
+    },
+    id: Math.floor(Math.random() * 100) + 1,
+  };
+
+  try {
+    const response = await axios.post(url, payload);
+    return response.data.result;
+  } catch (error) {
+    console.error("Error in fetchVehicleServices", error);
+    throw error;
+  }
+}
+
+export async function changeServiceState(
+  userId,
+  serviceId,
+  newState,
+  password
+) {
+  const url = `${BASE_URL}/jsonrpc`;
+  const pin = await SecureStore.getItemAsync("password");
+  const payload = {
+    jsonrpc: "2.0",
+    method: "call",
+    params: {
+      service: "object",
+      method: "execute_kw",
+      args: [
+        DB_NAME,
+        userId,
+        pin,
+        "fleet.vehicle.log.services", // Replace with your actual service model name
+        "write",
+        [[serviceId], { state: newState }], // Assuming 'state' is the field to be updated
+      ],
+    },
+    id: Math.floor(Math.random() * 100) + 1,
+  };
+
+  try {
+    const response = await axios.post(url, payload);
+    return response.data.result; // Indicate success
+  } catch (error) {
+    console.error("Error in changeServiceState", error);
+    throw error;
+  }
+}
+export async function createEmployeeCheckIn(employeeId, userId, password) {
+  const url = `${BASE_URL}/jsonrpc`;
+  const pin = await SecureStore.getItemAsync("password");
+
+  // Get current time in UTC and format it
+  const checkInTime = new Date().toISOString();
+  const formattedCheckInTime = checkInTime.replace('T', ' ').slice(0, 19);
+
+  console.log(formattedCheckInTime);
+
 
   const payload = {
     jsonrpc: "2.0",
@@ -86,11 +170,16 @@ export async function fetchVehicleServices(userId) {
       args: [
         DB_NAME,
         userId,
-        password,
-        'fleet.vehicle.log.services', 
-        'search_read',
-        [[]],
-        { fields: ['service_type_id', 'vehicle_id', 'date', 'purchaser_id', 'state'] }
+        pin,
+        "hr.attendance", // Assuming this is the model for check-ins
+        "create",
+        [
+          {
+            employee_id: parseInt(employeeId,10),
+            check_in: formattedCheckInTime, // Include the check-in time
+            // Add other relevant fields if needed
+          },
+        ],
       ],
     },
     id: Math.floor(Math.random() * 100) + 1,
@@ -98,10 +187,53 @@ export async function fetchVehicleServices(userId) {
 
   try {
     const response = await axios.post(url, payload);
-   
-    return response.data.result;
+    console.log("check in data " + response.data.result);
+    return response.data.result; // Return the result (e.g., ID of the created check-in record)
   } catch (error) {
-    console.error("Error in fetchVehicleServices", error);
+    console.error("Error in createEmployeeCheckIn", error);
     throw error;
-  } 
+  }
+}
+
+export async function createEmployeeCheckOut(checkInId, userId, password) {
+  const url = `${BASE_URL}/jsonrpc`;
+  const pin = await SecureStore.getItemAsync("password");
+
+  // Get current time in UTC and format it
+  const checkOutTime = new Date().toISOString();
+  const formattedCheckOutTime = checkOutTime.replace('T', ' ').slice(0, 19);
+
+  console.log(formattedCheckOutTime);
+
+  const payload = {
+    jsonrpc: "2.0",
+    method: "call",
+    params: {
+      service: "object",
+      method: "execute_kw",
+      args: [
+        DB_NAME,
+        userId,
+        pin,
+        "hr.attendance", // Assuming this is the model for check-outs
+        "write", // 'write' method is typically used for updating existing records
+        [
+          parseInt(checkInId, 10), // The ID of the check-in record to update
+          {
+            check_out: formattedCheckOutTime, // Include the check-out time
+          },
+        ],
+      ],
+    },
+    id: Math.floor(Math.random() * 100) + 1,
+  };
+
+  try {
+    const response = await axios.post(url, payload);
+    console.log("check out data " + response);
+    return response.data.result; // This might return a success status or the updated record ID
+  } catch (error) {
+    console.error("Error in createEmployeeCheckOut", error);
+    throw error;
+  }
 }
