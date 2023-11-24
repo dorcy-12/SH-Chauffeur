8b8702031fa1a0aa14c3e67d65c95b20721d5a0d
dhakizimana <agapedorcy@gmail.com>
2023-11-24 12:40:45 +0100

    documents scrreen clearred

--- screens/documents.js
+++ screens/documents.js
@@ -2,38 +2,23 @@ import React, { useState, useEffect, useMemo } from "react";
 import { View, Text, ScrollView, StyleSheet, StatusBar } from "react-native";
 import { useTheme } from "../context/ThemeContext";
 import { Ionicons } from "@expo/vector-icons";
-import TripCard from "../Components/TripCard";
+import TripCard from "../Components/DriveCard";
 import * as SecureStore from "expo-secure-store";
-import { useTrip } from "../context/TripContext";
-
+import { useService } from "../context/ServiceContext";
 
 const DocumentsScreen = () => {
   const theme = useTheme();
   const styles = createStyles(theme);
-  const {trips, setTrips, allTrips,setAllTrips} = useTrip();
-  const finishedTrips = useMemo(
-  () => allTrips.filter((trip) => trip.is_completed),
-  [allTrips]
-  );
-   
+  const { activeService, setActiveService, allServices, setAllServices } = useService();
+
+
   return (
     <ScrollView style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.title}>Fahrten</Text>
       </View>
       <View style={styles.content}>
-        {finishedTrips.map((trip, index) => (
-          <TripCard
-            key={index}
-            trip={{
-              carNumber: trip.vehicle.vehicle_number,
-              startTime: new Date(trip.start_time).toLocaleTimeString(),
-              endTime: new Date(trip.stopped).toLocaleTimeString(),
-              date: new Date(trip.start_time).toLocaleDateString(),
-              kilometers: trip.distance,
-            }}
-          />
-        ))}
+        <Text>Hi</Text>
       </View>
     </ScrollView>
   );
