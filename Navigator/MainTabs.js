0c7437f80f7643d0e8067bc8c4acd47498d4f014
dorcy-12 <agapedorcy@gmail.com>
2023-12-19 12:30:41 +0100

    simplified the tabs

--- Navigator/MainTabs.js
+++ Navigator/MainTabs.js
@@ -3,16 +3,84 @@ import Footer from "../Components/footer";
 import HomeScreen from "../screens/home";
 import ProfileScreen from "../screens/profile";
 import DocumentsScreen from "../screens/documents";
+import { Ionicons } from "@expo/vector-icons";
+import { useTheme } from "../context/ThemeContext";
 const Tab = createBottomTabNavigator();
 
 function MainTabs() {
+  const theme = useTheme();
   return (
     <Tab.Navigator
-      tabBar={(props) => <Footer {...props} />}
+      screenOptions={{
+        tabBarActiveTintColor: theme.secondary,
+        tabBarStyle: {
+          backgroundColor: "white",
+          //borderTopColor: "grey", // Set the background color to black
+        },
+      }}
     >
-      <Tab.Screen name="home" component={HomeScreen} options={{headerShown:false}}/>
-      <Tab.Screen name="profile" component={ProfileScreen} options={{headerShown:false}}/>
-      <Tab.Screen name="documents" component={DocumentsScreen} options={{headerShown:false}}/>
+      <Tab.Screen
+        name="home"
+        component={HomeScreen}
+        options={{
+          headerShown: false,
+          tabBarLabel: "Home",
+          tabBarIcon: ({ focused, color, size }) => {
+            // Reduce the size of the icons
+            const iconSize = size * 0.8; // Adjust the factor to make icons smaller
+            // Return the icon component
+            return (
+              <Ionicons
+                name="home"
+                color={color}
+                size={focused ? size : iconSize}
+              />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="profile"
+        component={ProfileScreen}
+        options={{
+          headerShown: false,
+          tabBarLabel: "Profil",
+          tabBarIcon: ({ focused, color, size }) => {
+            // Reduce the size of the icons
+            const iconSize = size * 0.8; // Adjust the factor to make icons smaller
+            // Return the icon component
+            return (
+              <Ionicons
+                name="person"
+                color={color}
+                size={focused ? size : iconSize}
+                focused
+              />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="documents"
+        component={DocumentsScreen}
+        options={{
+          headerShown: false,
+          tabBarLabel: "Verlauf",
+          tabBarIcon: ({ focused, color, size }) => {
+            // Reduce the size of the icons
+            const iconSize = size * 0.8; // Adjust the factor to make icons smaller
+            // Return the icon component
+            return (
+              <Ionicons
+                name="time"
+                color={color}
+                size={focused ? size : iconSize}
+                focused
+              />
+            );
+          },
+        }}
+      />
       {/* Add more Tab.Screen components as needed */}
     </Tab.Navigator>
   );
