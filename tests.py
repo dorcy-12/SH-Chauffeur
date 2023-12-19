7eb018658d8e9b5f11b3dfcc13abafadc031ac5b
dorcy-12 <agapedorcy@gmail.com>
2023-12-19 11:55:44 +0100

    new tests

--- tests.py
+++ tests.py
@@ -1,58 +1,44 @@
-import ssl
-import certifi
 import xmlrpc.client
 from datetime import datetime
 dorcy, dorcypw = "h.dorcy@sh-personal.com", "amatama"
-admin, adminpw = "default@localhost.localdomain", "%z1S5#t_Za"
-
-#context = ssl.create_default_context(cafile=certifi.where())
-#info = xmlrpc.client.ServerProxy('https://demo.odoo.com/start', context = context).start()
-#url,db, username, password = info['host'], info['database'], info['user'], info['password']
+admin, adminpw = "default@localhost.localdomain", "9N*Aj8#tW9"
 
 url = "http://217.160.15.116"
-db = "default_sx3p1odoo"
+db = "default_xnqp1odoo"
 username = dorcy
 password = dorcypw
 
 common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(url))
 uid = common.authenticate(db, username, password, {})
+print(uid)
 models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(url))
 
 def get_all_employees():
-    employees = models.execute_kw(
-        db, uid, password,
-        'hr.employee', 'search_read',
-        [[]],  # Empty list for search domain to get all records
-        {'fields': ['id', 'name']}  # Fields you want to fetch, such as 'id' and 'name'
-    )
+    try:
+        employees = models.execute_kw(
+            db, uid, password,
+            'hr.employee', 'search_read',
+            [[]],  # Empty list for search domain to get all records
+            {'fields': ['id', 'name']}  # Fields you want to fetch, such as 'id' and 'name'
+        )
 
-    for employee in employees:
-        print(f"Employee ID: {employee['id']}, Name: {employee['name']}")
-#
-get_all_employees()
+        for employee in employees:
+            print(f"Employee ID: {employee['id']}, Name: {employee['name']}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
-#models_list = models.execute_kw(
-#    db, uid, password,
-#    'ir.model', 'search_read',
-#    [[]],
-#   {'fields': ['model']}
-#)
+#get_all_employees()
 
-#for model in models_list:
-#    print(model['model'])
-
-# Search for installed apps
-
-#service_records = models.execute_kw(
-#    db, uid, password,
-#   'fleet.vehicle.log.services', 'search_read',
-#    [[]],
-#    {'fields': ['service_type_id','vehicle_id', 'amount', 'date', 'purchaser_id','state']}  # Adjust the fields based on your needs
-#)
-
-#Printing details of each service record
-#for record in service_records:
-    #print(f"Service Type: {record['service_type_id']},Date: {record['date']}, Car: {record['vehicle_id']}, driver:{record['purchaser_id']}, state: {record['state']}")
+if uid:
+    user_data = models.execute_kw(db, uid, password, 'res.users', 'read', [uid], {'fields': ['partner_id']})
+    
+    if user_data and user_data[0]:
+        partner_id = user_data[0]['partner_id'][0]  # The first element is the ID
+        print("Your partner ID is:", partner_id)
+    else:
+        print("User data not found.")
+else:
+    print("Authentication failed.")
 
 def check_in():
     check_in_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
@@ -64,4 +50,38 @@ def check_in():
     print(f"Checked in with ID: {attendance_id}")
 
 employee_id = 4  # Replace with the actual employee ID
-check_in()
+
+
+def upsert_firebase_token(partner_id, firebase_token, device_os):
+    if uid:
+        # Search for existing token for the partner
+        existing_token_id = models.execute_kw(
+            db, uid, password,
+            'mail.firebase', 'search',
+            [[['partner_id', '=', partner_id], ['os', '=', device_os]]]
+        )
+
+        if existing_token_id:
+            # Update existing token
+            response = models.execute_kw(
+                        db, uid, password,
+                        'mail.firebase', 'write',
+                        [existing_token_id, {'token': firebase_token}]
+                    )
+            print(f"Token updated for partner ID:{partner_id} AND RESPONSE IS {response} ")
+        else:
+            # Create new token
+            response = models.execute_kw(
+                        db, uid, password,
+                        'mail.firebase', 'create',
+                        [{'partner_id': partner_id, 'token': firebase_token, 'os': device_os}]
+                    )
+            print(f"Token created for partner ID:{partner_id} amd response id {response}")
+    else:
+        print("Authentication Failed")
+
+# Example usage
+partner_id = 12  # Replace with the actual partner ID
+firebase_token = 'ambatam'  # Replace with the actual Firebase token
+device_os = 'Sionooo'  # Example device OS
+#upsert_firebase_token(partner_id, firebase_token, device_os)
