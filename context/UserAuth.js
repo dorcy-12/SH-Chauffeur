0d3d99a2ad3589288664604ad6988231dbdab8dd
dhakizimana <agapedorcy@gmail.com>
2024-01-04 12:23:09 +0100

    new context variables

--- context/UserAuth.js
+++ context/UserAuth.js
@@ -3,12 +3,16 @@ import { createContext, useState } from 'react';
 export const AuthContext = createContext({
   isUserLoggedIn: false,
   userId: null,
-  employeeId: null,
+  partnerId:null,
+  employeeProfile: null,
   password:null,
   shouldReloadServices: false,
+  channels: null,
   setIsUserLoggedIn: () => {},
   setUserId: () => {},
-  setEmployeeId: () =>{},
+  setPartnerId: () => {},
+  setEmployeeProfile: () =>{},
   setPassword: () => {},
   setShouldReloadServices: () => {},
+  setChannels: () =>{},
 });
